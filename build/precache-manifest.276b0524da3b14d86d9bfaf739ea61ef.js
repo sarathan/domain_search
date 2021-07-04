5aed68b9810cd0bf85329a98c1c5c8f40ce3ff0d
sarathan <sarath.annamraju@gmail.com>
2021-07-04 15:48:36 +0530

    deploy

---
+++ build/precache-manifest.276b0524da3b14d86d9bfaf739ea61ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ba5353ac48cc0d55761d2353183af7",
+    "url": "/TechnologySearch/index.html"
+  },
+  {
+    "revision": "2b6f2e14e2a1f5adf2dc",
+    "url": "/TechnologySearch/static/css/main.64e917aa.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/TechnologySearch/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2b6f2e14e2a1f5adf2dc",
+    "url": "/TechnologySearch/static/js/main.d8c15d94.chunk.js"
+  },
+  {
+    "revision": "802a86bd3ca1f793bf4b",
+    "url": "/TechnologySearch/static/js/runtime~main.90d5fa06.js"
+  }
+]);
