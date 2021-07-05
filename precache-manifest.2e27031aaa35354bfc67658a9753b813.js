af6c3f5b74e87c3e85409f5b692f9ea065fc6de4
sarathan <sarath.annamraju@gmail.com>
2021-07-06 01:07:20 +0530

    Updates

---
+++ precache-manifest.2e27031aaa35354bfc67658a9753b813.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214fa13200a2e383f8c257030673c222",
+    "url": "/domain-search/index.html"
+  },
+  {
+    "revision": "2b6f2e14e2a1f5adf2dc",
+    "url": "/domain-search/static/css/main.64e917aa.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/domain-search/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2b6f2e14e2a1f5adf2dc",
+    "url": "/domain-search/static/js/main.d8c15d94.chunk.js"
+  },
+  {
+    "revision": "395046c090556b2cc56c",
+    "url": "/domain-search/static/js/runtime~main.2141f74a.js"
+  }
+]);
